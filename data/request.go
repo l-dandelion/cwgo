39e96c5f3ade089cf9a2f4bab188c8a112bf41b7
张立伟 <zhangliweibackend@bytedance.com>
2018-06-02 23:37:59 +0800

    add data(req, resp, item)

---
+++ data/request.go
@@ -0,0 +1,82 @@
+package data
+
+import (
+	"net/http"
+)
+
+type Request struct {
+	httpReq *http.Request
+	depth   int                    // 抓取深度
+	proxy   string                 // 代理信息
+	Extra   map[string]interface{} // 额外信息
+}
+
+/*
+ * New an instance of Request
+ */
+func NewRequest(httpReq *http.Request, extras ...map[string]interface{}) *Request {
+	var extra map[string]interface{}
+	if len(extras) != 0 {
+		extra = extras[0]
+	} else {
+		extra = map[string]interface{}{}
+	}
+	return &Request{
+		httpReq: httpReq,
+		Extra:   extra,
+	}
+}
+
+/*Get*/
+
+func (req *Request) HTTPReq() *http.Request {
+	return req.httpReq
+}
+
+func (req *Request) Depth() int {
+	return req.depth
+}
+
+func (req *Request) Proxy() string {
+	return req.proxy
+}
+
+func (req *Request) SetExtra(key string, val interface{}) {
+	req.Extra[key] = val
+}
+
+/*Set*/
+
+func (req *Request) SetDepth(depth int) {
+	req.depth = depth
+}
+
+func (req *Request) SetHeader(key, val string) {
+	req.httpReq.Header.Set(key, val)
+}
+
+//cookie
+func (req *Request) AddCookie(key, value string) {
+	c := &http.Cookie{
+		Name:  key,
+		Value: value,
+	}
+	req.httpReq.AddCookie(c)
+}
+
+//ua
+func (req *Request) SetUserAgent(ua string) {
+	req.SetHeader("User-Agent", ua)
+}
+
+//referer
+func (req *Request) SetReferer(referer string) {
+	req.SetHeader("referer", referer)
+}
+
+//proxy
+func (req *Request) SetProxy(proxy string) {
+	req.proxy = proxy
+}
+
+
---
+++ data/response.go
@@ -0,0 +1,110 @@
+package data
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/cwgo/lib/reader"
+	"golang.org/x/net/html/charset"
+)
+
+type Response struct {
+	req      *Request          //响应对应的请求
+	httpResp *http.Response    //请求对应的http响应
+	text     []byte            //body的[]byte类型
+	dom      *goquery.Document //dom结构
+}
+
+/*
+ * New an instance of Response
+ */
+func NewResponse(req *Request, httpResp *http.Response) *Response {
+	return &Response{req: req, httpResp: httpResp}
+}
+
+/*Get*/
+
+func (resp *Response) HTTPRequest() *http.Request {
+	return resp.req.HTTPReq()
+}
+
+func (resp *Response) HTTPResp() *http.Response {
+	return resp.httpResp
+}
+
+func (resp *Response) Depth() int {
+	return resp.req.Depth()
+}
+
+func (resp *Response) GetText() ([]byte, error) {
+	if resp.text != nil {
+		return resp.text, nil
+	}
+	multiReader, err := reader.NewMultipleReader(resp.httpResp.Body)
+	resp.httpResp.Body = multiReader.Reader()
+	defer func() {
+		resp.httpResp.Body.Close()
+		resp.httpResp.Body = multiReader.Reader()
+	}()
+	var contentType, pageEncode string
+
+	// read firstly content-type from response header
+	contentType = resp.httpResp.Header.Get("Content-Type")
+	if _, params, err := mime.ParseMediaType(contentType); err == nil {
+		if cs, ok := params["charset"]; ok {
+			pageEncode = strings.ToLower(strings.TrimSpace(cs))
+		}
+	}
+
+	// read content-type from request header
+	if len(pageEncode) == 0 {
+		contentType = resp.httpResp.Request.Header.Get("Content-Type")
+		if _, params, err := mime.ParseMediaType(contentType); err == nil {
+			if cs, ok := params["charset"]; ok {
+				pageEncode = strings.ToLower(strings.TrimSpace(cs))
+			}
+		}
+	}
+
+	switch pageEncode {
+	case "utf8", "utf-8", "unicode-1-1-utf-8":
+	default:
+		// get converter to utf-8
+		// Charset auto determine. Use golang.org/x/net/html/charset. Get response body and change it to utf-8
+		var destReader io.Reader
+
+		if len(pageEncode) == 0 {
+			destReader, err = charset.NewReader(resp.httpResp.Body, "")
+		} else {
+			destReader, err = charset.NewReaderLabel(pageEncode, resp.httpResp.Body)
+		}
+
+		if err == nil {
+			resp.text, err = ioutil.ReadAll(destReader)
+			if err == nil {
+				return resp.text, err
+			}
+		}
+
+	}
+	resp.text, err = ioutil.ReadAll(resp.httpResp.Body)
+
+	return resp.text, err
+}
+
+func (resp *Response) GetDom() (*goquery.Document, error) {
+	if resp.dom != nil {
+		return resp.dom, nil
+	}
+	text, err := resp.GetText()
+	if err != nil {
+		return nil, err
+	}
+	resp.dom, err = goquery.NewDocumentFromReader(bytes.NewReader(text))
+	return resp.dom, err
+}
